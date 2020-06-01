1890fe6af51de320593bf565d72f6d564842e1a4
zhaotong <18513865095@163.com>
2020-06-01 16:14:46 +0800

    测试

--- src/main/java/com/bdhlife/controller/GoodsController.java
+++ src/main/java/com/bdhlife/controller/GoodsController.java
@@ -7,12 +7,14 @@ import com.bdhlife.service.GoodsService;
 import com.bdhlife.service.PictureService;
 import com.bdhlife.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/goods")
 public class GoodsController {
 
